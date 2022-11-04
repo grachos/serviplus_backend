ac6e60abaa2af3d137e4b661252da9b8a6fe61d0
grachos <graziany@gmail.com>
2022-11-03 21:46:55 -0500

    Update: Conexion DB online

--- database.js
+++ database.js
@@ -2,7 +2,9 @@ const mongo = require("mongoose");
 
 (async ()=> {
     try{
-        const db = await mongo.connect("mongodb://localhost:27017/serviplusdb");
+         const db = await mongo.connect("mongodb://localhost:27017/serviplusdb");
+        
+        //const db = await mongo.connect("mongodb+srv://grachos:6nQLOq47uXXtTe4K@cluster0.3za8nzp.mongodb.net/serviplusdb?retryWrites=true&w=majority");
         console.log("Fue realizada la conexión " + db.connection.name);
     }catch(error){
         console.error(error);
