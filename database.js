33f287dfe8adf786bc15823feec03363a43e98f2
grachos <graziany@gmail.com>
2022-11-08 20:33:32 -0500

    Despliegue No 1

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 const mongo = require("mongoose");
 
-(async ()=> {
-    try{
-         const db = await mongo.connect("mongodb://localhost:27017/serviplusdb");
-        
-        //const db = await mongo.connect("mongodb+srv://grachos:6nQLOq47uXXtTe4K@cluster0.3za8nzp.mongodb.net/serviplusdb?retryWrites=true&w=majority");
+(async () => {
+    try {
+        //const db = await mongo.connect("mongodb://localhost:27017/serviplusdb"); //Locall
+
+        //Remoto
+        const db = await mongo.connect("mongodb+srv://grachos:6nQLOq47uXXtTe4K@cluster0.3za8nzp.mongodb.net/serviplusdb?retryWrites=true&w=majority");
         console.log("Fue realizada la conexión " + db.connection.name);
-    }catch(error){
+    } catch (error) {
         console.error(error);
     }
 })();
